de01daa9b18f10099e6cf531364bf8a66d352722
Garlic Toum <55115386+GarlicToum@users.noreply.github.com>
2020-01-22 17:33:12 -0600

    hacked together arduino.py to get ulp working

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -22,15 +22,81 @@ kinds of creative coding, interactive objects, spaces or physical experiences.
 http://arduino.cc/en/Reference/HomePage
 """
 
-from os.path import join
 
-from SCons.Script import DefaultEnvironment, SConscript
+import sys
+from os import listdir, walk
+from os.path import abspath, basename, isdir, isfile, join
+
+from SCons.Script import DefaultEnvironment
 
 env = DefaultEnvironment()
 
-SConscript("_embed_files.py", exports="env")
+ulp_lib = None
+ulp_dir = join(env.subst("$PROJECT_DIR"), "ulp")
+if isdir(ulp_dir) and listdir(ulp_dir):
+    ulp_lib = env.SConscript("ulp.py", exports="env")
+
+env.SConscript("_embed_files.py", exports="env")
 
 if "espidf" not in env.subst("$PIOFRAMEWORK"):
-    SConscript(
-        join(DefaultEnvironment().PioPlatform().get_package_dir(
-            "framework-arduinoespressif32"), "tools", "platformio-build.py"))
+    env.SConscript(
+        join(
+            DefaultEnvironment()
+            .PioPlatform()
+            .get_package_dir("framework-arduinoespressif32"),
+            "tools",
+            "platformio-build.py",
+        )
+    )
+
+
+def get_sdk_configuration(config_path):
+    if not isfile(config_path):
+        sys.stderr.write('Error: Could not find "sdkconfig.h" file\n')
+        env.Exit(1)
+
+    config = {}
+    with open(config_path) as fp:
+        for l in fp.readlines():
+            if not l.startswith("#define"):
+                continue
+            values = l.split()
+            config[values[1]] = values[2]
+
+    return config
+
+
+def is_set(parameter, configuration):
+    if int(configuration.get(parameter, 0)):
+        return True
+    return False
+
+
+def is_ulp_enabled(sdk_params):
+    ulp_memory = int(sdk_params.get("CONFIG_ULP_COPROC_RESERVE_MEM", 0))
+    ulp_enabled = is_set("CONFIG_ULP_COPROC_ENABLED", sdk_params)
+    return ulp_memory > 0 and ulp_enabled
+
+
+sdk_config_header = join(env.subst("$PROJECTSRC_DIR"), "sdkconfig.h")
+sdk_params = get_sdk_configuration(sdk_config_header)
+
+libs = []
+
+if ulp_lib:
+    if not is_ulp_enabled(sdk_params):
+        print(
+            "Warning! ULP is not properly configured."
+            "Add next configuration lines to your sdkconfig.h:"
+        )
+        print("    #define CONFIG_ULP_COPROC_ENABLED 1")
+        print("    #define CONFIG_ULP_COPROC_RESERVE_MEM 1024")
+
+    libs.append(ulp_lib)
+    env.Append(
+        CPPPATH=[join("$BUILD_DIR", "ulp_app")],
+        LIBPATH=[join("$BUILD_DIR", "ulp_app")],
+        LINKFLAGS=["-T", "ulp_main.ld"],
+    )
+
+env.Prepend(LIBS=libs)
